41ac5f3edf0f3f0e852f1e3b465ea7f127c144e9
tatianaYa16 <tatianakrus@yandex.ru>
2022-06-30 00:04:45 +0300

    fix commets

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import styles from ".//burger_constructor.module.css";
+import styles from "./burger-constructor.module.css";
 import {ConstructorElement} from "@ya.praktikum/react-developer-burger-ui-components";
 import {DragIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {Button} from '@ya.praktikum/react-developer-burger-ui-components';
 import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 import PropTypes from 'prop-types';
+import Modal from "../modal/modal";
+import OrderDetails from "../order-details/order-details";
 
 BurgerConstructor.propTypes = {
     items: PropTypes.array
@@ -13,23 +15,44 @@ BurgerConstructor.propTypes = {
 
 export default function BurgerConstructor(props) {
     const [orderCost, setOrderCost] = useState(0);
+    const [modal, setModal] = useState(false);
 
-    console.log("BurgerConstructor :" + props.items);
+    const handleOpenModal = () => {
+        setModal(!modal);
+    }
+
     return (
-        <section>
-            <div className={styles.ingredients}>
-                {props.items.map((ingredient, index) => (
-                    <div>
-                        <DragIcon type="primary"/>
-                        <ConstructorElement
-                            type={ingredient.type}
-                            isLocked={false}
-                            text={ingredient.name}
-                            price={ingredient.price}
-                            thumbnail={ingredient.image}
-                        />
-                    </div>
-                ))}
+        <section className={styles.constructorMain}>
+            <div className={styles.buns +'pl-10'}>
+                <ConstructorElement
+                    type={props.items[0].type}
+                    isLocked={true}
+                    text={props.items[0].name + ' верх'}
+                    price={props.items[0].price}
+                    thumbnail={props.items[0].image}
+                />
+                <div className={styles.ingredients}>
+                    {props.items.map((ingredient) => (
+                        <div key={ingredient._id}>
+                            <DragIcon type="primary"/>
+                            <ConstructorElement
+                                type={ingredient.type}
+                                isLocked={false}
+                                text={ingredient.name}
+                                price={ingredient.price}
+                                thumbnail={ingredient.image}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <ConstructorElement
+                    type={props.items[0].type}
+                    isLocked={true}
+                    text={props.items[0].name + ' низ'}
+                    price={props.items[0].price}
+                    thumbnail={props.items[0].image}
+                />
+
 
             </div>
             <div className={styles.order}>
@@ -37,11 +60,16 @@ export default function BurgerConstructor(props) {
                     {orderCost}
                 </p>
                 <CurrencyIcon type="primary"/>
-                <Button type="primary" size="large">
+                <Button type="primary" size="large" onClick={handleOpenModal}>
                     Оформить заказ
                 </Button>
             </div>
 
+            {modal &&
+                <Modal onClose={handleOpenModal}>
+                    <OrderDetails />
+                </Modal>
+            }
         </section>
     )
 };
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -4,15 +4,17 @@ import styles from './burger-ingredients.module.css';
 import PropTypes from "prop-types";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import IngredientDetails from "../ingredient-details/ingredient-details"
+import Modal from "../modal/modal";
 
 BurgerConstructor.propTypes = {
-    items: PropTypes.array
+    items: PropTypes.array.isRequired
 };
 
 const MenuTab = () => {
     const [current, setCurrent] = React.useState('one')
     return (
-        <div style={{display: 'flex'}}>
+        <div className={styles.menu}>
             <Tab value="one" active={current === 'one'} onClick={setCurrent}>
                 Булки
             </Tab>
@@ -28,13 +30,13 @@ const MenuTab = () => {
 
 const Ingridient = (props) => {
     return (
-        <div>
+        <div onClick={props.onClick} id={props.item._id} className={styles.ingredientCard}>
             <img src={props.item.image_large} className={styles.img}/>
             <div className={styles.price}>
                 <p className="text text_type_digits-default">{props.item.price}</p>
-                <CurrencyIcon type="primary" />
+                <CurrencyIcon type="primary"/>
             </div>
-            <p className="text text_type_main-small">
+            <p className={styles.name + ' text text_type_main-small '}>
                 {props.item.name}
             </p>
         </div>
@@ -46,40 +48,68 @@ export default function BurgerIngredients(props) {
     const mains = props.items.filter(item => item.type === "main");
     const sauces = props.items.filter(item => item.type === "sauce");
 
+    const [visibleModal, setVisibleModal] = React.useState(false);
+    const [selectedIngredient, setSelectedIngredient] = React.useState({});
+
+    const handleOpenModal = (e) => {
+        const target = e.currentTarget;
+        console.log(target);
+        const id = target.getAttribute('id');
+        console.log("id : " + id);
+        setSelectedIngredient(props.items.find((item) => item._id === id));
+        setVisibleModal(true);
+        console.log(selectedIngredient.name);
+    }
+
+    const handleCloseModal = () => {
+        setVisibleModal(false);
+    }
+
     return (
-        <div className={styles.ingredients}>
-            <span>Соберите бургер</span>
-            <MenuTab/>
-            <p className="text text_type_main-medium text_left">
-                Булки
-            </p>
-            <div className={styles.ingredientItem}>
-                {buns.map((bun, index) => (
+        <section>
+            <div className='pt-10 pb-10'>
+                <p className="text text_type_main-large text_left">
+                    Соберите бургер
+                </p>
+                <MenuTab/>
+                <div className={styles.ingredients}>
                     <div>
-                        <Ingridient key={index} item={bun}/>
+                        <p className="text text_type_main-medium text_left">
+                            Булки
+                        </p>
+                        <div className={styles.ingredientItem}>
+                            {buns.map((bun) => (
+                                <Ingridient key={bun._id} item={bun} onClick={handleOpenModal}/>
+                            ))}
+                        </div>
                     </div>
-                ))}
-            </div>
-            <p className="text text_type_main-medium text_left">
-                Соусы
-            </p>
-            <div className={styles.ingredientItem}>
-                {sauces.map((sauce, index) => (
                     <div>
-                        <Ingridient key={index} item={sauce}/>
+                        <p className="text text_type_main-medium text_left">
+                            Соусы
+                        </p>
+                        <div className={styles.ingredientItem}>
+                            {sauces.map((sauce) => (
+                                <Ingridient key={sauce._id} item={sauce} onClick={handleOpenModal}/>
+                            ))}
+                        </div>
                     </div>
-                ))}
-            </div>
-            <p className="text text_type_main-medium text_left">
-                Начинки
-            </p>
-            <div className={styles.ingredientItem}>
-                {mains.map((main, index) => (
                     <div>
-                        <Ingridient key={index} item={main}/>
+                        <p className="text text_type_main-medium text_left">
+                            Начинки
+                        </p>
+                        <div className={styles.ingredientItem}>
+                            {mains.map((main) => (
+                                <Ingridient key={main._id} item={main} onClick={handleOpenModal}/>
+                            ))}
+                        </div>
                     </div>
-                ))}
+                </div>
             </div>
-        </div>
+                {visibleModal && selectedIngredient &&
+                    <Modal onClose={handleCloseModal} headerText="Детали ингредиента">
+                        <IngredientDetails item={selectedIngredient}/>
+                    </Modal>
+                }
+        </section>
     )
 };
